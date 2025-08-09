c484d7cfbe735523966194d94ddd5b91ebbe6f3c
juanhubertl8010 <juanhl8010@gmail.com>
2025-08-09 10:48:44 +0700

    first commit

--- Project1Hackingwithswift/ContentView.swift
+++ Project1Hackingwithswift/ContentView.swift
@@ -8,14 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var amount = 0.0
+    @State private var amountofpeople = 2
+    @State private var percenttips = 10
+    let percentagetips = [10, 20, 50, 100]
+    
+    var totalperson: Double {
+        let totalbill = Double(amount)
+        let totalperson = Double(amountofpeople)+2
+        let percentage = Double(percenttips)
+        let tips = totalbill * (percentage/100)
+        let grandtotal = totalbill + tips
+        let amontperperson = grandtotal/totalperson
+        
+        return amontperperson
+        
+    }
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            Form{
+                Section{
+                    TextField("Amount", value: $amount, format: .currency(code: "USD"))
+                    Picker("Total of People", selection: $amountofpeople ){
+                        ForEach(2..<100)
+                        {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                    Section{
+                        Picker("Tip", selection: $percenttips)
+                        {
+                            ForEach(percentagetips, id: \.self)
+                            {
+                                Text("\($0)%")
+                            }
+                            
+                        }
+                        .pickerStyle(.segmented)
+                    }
+                Section{
+                    Text("\(totalperson)")
+                }
+                
+            }
+            .navigationTitle("We Spilt")
         }
-        .padding()
+        
     }
 }
 
