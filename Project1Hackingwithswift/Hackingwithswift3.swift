8c2a4d1a773d491008fa74c1d4dda3937e47d270
juanhubertl8010 <juanhl8010@gmail.com>
2025-08-10 10:57:10 +0700

    Feat: Hackingproject3

---
+++ Project1Hackingwithswift/Hackingwithswift3.swift
@@ -0,0 +1,18 @@
+//
+//  Hackingwithswift3.swift
+//  Project1Hackingwithswift
+//
+//  Created by Juan Hubert Liem on 10/08/25.
+//
+
+import SwiftUI
+
+struct Hackingwithswift3: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Hackingwithswift3()
+}
