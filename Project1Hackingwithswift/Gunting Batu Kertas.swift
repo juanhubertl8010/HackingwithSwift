361e7276bb5b71c4cc9984eb0794cbe5980a04a4
juanhubertl8010 <juanhl8010@gmail.com>
2025-08-10 18:16:50 +0700

    Feat: Batu Gunting Kertas

---
+++ Project1Hackingwithswift/Gunting Batu Kertas.swift
@@ -0,0 +1,18 @@
+//
+//  Gunting Batu Kertas.swift
+//  Project1Hackingwithswift
+//
+//  Created by Juan Hubert Liem on 10/08/25.
+//
+
+import SwiftUI
+
+struct Gunting_Batu_Kertas: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Gunting_Batu_Kertas()
+}
